a8198a3ede305682fddcf50e05cc560c40ec1372
jairus-m <jairusmartinez@gmail.com>
2025-03-27 10:20:34 -0700

    Modufy returned output + update docstrings

--- dagster_dlt/defs/assets/dlt/1_poke_rest_api.py
+++ dagster_dlt/defs/assets/dlt/1_poke_rest_api.py
@@ -1,46 +1,53 @@
+"""
+This example uses the dlt library within the Dagster framework (non-integration) to materialize
+multi-assets from 3 endpoints of a single REST API source.
+
+The dltHub example is pulled directly from: https://dlthub.com/docs/tutorial/rest-api.
+  - The dltHub code is simply wrapped with the @dg.multi_asset decorator
+"""
+
 import dagster as dg
 import dlt
 from dlt.sources.rest_api import rest_api_source
 
+
 @dg.multi_asset(
     outs={
         "pokemon": dg.AssetOut(
-            key=[
+            key=[ # asset key becomes the table name in target DB
                 "poke_api",
                 "pokemon",
-            ],  # asset key becomes the table name in target DB
-            description="General Pokemon data",
+            ],  
+            description="General Pokemon data", # description associated with asset, viewable in Dagster UI
         ),
         "berry": dg.AssetOut(
             key=[
                 "poke_api",
                 "berry",
-            ],  
+            ],
             description="Berry data which provide HP and status condition restoration, stat enhancement, and even damage negation when eaten by Pokémon",
         ),
         "location": dg.AssetOut(
             key=[
                 "poke_api",
                 "location",
-            ], 
+            ],
             description="Locations that exist within Pokemon games",
-        ),        
+        ),
     },
-    group_name="dltHub__github",
+    group_name="dltHub__github_1",
     compute_kind="dlt",
 )
 def load_pokemon():
     pipeline = dlt.pipeline(
-        pipeline_name="rest_api_pokemon",
+        pipeline_name="example_1",
         destination="duckdb",
-        dataset_name="rest_api_data",
+        dataset_name="poke_rest_api_1",
     )
 
     pokemon_source = rest_api_source(
         {
-            "client": {
-                "base_url": "https://pokeapi.co/api/v2/"
-            },
+            "client": {"base_url": "https://pokeapi.co/api/v2/"},
             "resource_defaults": {
                 "endpoint": {
                     "params": {
@@ -59,6 +66,10 @@ def load_pokemon():
     load_info = pipeline.run(pokemon_source)
     print(load_info)
 
-    yield dg.Output(value=load_info[0], output_name="pokemon")
-    yield dg.Output(value=load_info[1], output_name="berry")
-    yield dg.Output(value=load_info[2], output_name="location")
+    # when an op is defined to have multiple outputs, Dagster expects the function
+    # to either yield each output individually or return a tuple containing a value for each output
+    return (
+        dg.Output(value=load_info[0]), # returning dlt load_info object corresponding to each resource (pokemon)
+        dg.Output(value=load_info[1]), # berry
+        dg.Output(value=load_info[2]), # location
+    )
